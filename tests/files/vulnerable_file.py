c14a1bc867fb916b932dae40154d6ae457bae62b
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-06-23 18:32:03 +0100

    dummy vumnerable testing file.

---
+++ tests/files/vulnerable_file.py
@@ -0,0 +1,14 @@
+def logging(url, api_key):
+    print('Connecting...')
+    print('Connected successfully !')
+
+
+def connect(url):
+    api_key = "5729-4ba9-8c08-f2df493465a1"
+    logging(url, api_key)
+
+
+if __name__ == '__main__':
+    connect('https://www.dummy.com/')
+
+
